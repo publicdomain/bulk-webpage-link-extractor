7332ac7ff719463f6df95d1ca4c2345f8641b7d8
Victor L. Senior <publicdomainvic@gmail.com>
2019-10-11 06:18:25 -0400

    Add initial project files

---
+++ BulkWebpageLinkExtractor/MainForm.cs
@@ -0,0 +1,128 @@
+﻿// <copyright file="MainForm.cs" company="PublicDomain.com">
+//     CC0 1.0 Universal (CC0 1.0) - Public Domain Dedication
+//     https://creativecommons.org/publicdomain/zero/1.0/legalcode
+// </copyright>
+
+namespace BulkWebpageLinkExtractor
+{
+    // Directives
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Description of MainForm.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:BulkWebpageLinkExtractor.MainForm"/> class.
+        /// </summary>
+        public MainForm()
+        {
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the extract button click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnExtractButtonClick(object sender, EventArgs e)
+        {
+            // TODO Add code.
+        }
+
+        /// <summary>
+        /// Handles the copy button click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnCopyButtonClick(object sender, EventArgs e)
+        {
+            // TODO Add code.
+        }
+
+        /// <summary>
+        /// Handles the save button click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnSaveButtonClick(object sender, EventArgs e)
+        {
+            // TODO Add code.
+        }
+
+        /// <summary>
+        /// Handles the result rich text box link clicked event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnResultRichTextBoxLinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            // TODO Add code.
+        }
+
+        /// <summary>
+        /// Handles the new tool strip menu item click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnNewToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code.
+        }
+
+        /// <summary>
+        /// Handles the exit tool strip menu item click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnExitToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code.
+        }
+
+        /// <summary>
+        /// Handles the options tool strip menu item drop down item clicked event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnOptionsToolStripMenuItemDropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            // TODO Add code.
+        }
+
+        /// <summary>
+        /// Handles the headquarters patreon.com tool strip menu item click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnHeadquartersPatreoncomToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code.
+        }
+
+        /// <summary>
+        /// Handles the source code github.com tool strip menu item click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code.
+        }
+
+        /// <summary>
+        /// Handles the original thread reddit.com tool strip menu item click event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnOriginalThreadRedditcomToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code.
+        }
+    }
+}
